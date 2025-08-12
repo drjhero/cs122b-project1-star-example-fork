98333007feefa50762bbacd70bfd4768f26d4b62
drjhero <james.thomas.herold@gmail.com>
2025-08-11 20:24:13 -0700

    Made some minor nitpick changes to the code. Added try with resources. Changed password.

--- src/StarServlet.java
+++ src/StarServlet.java
@@ -12,90 +12,67 @@ import java.sql.Statement;
 // This annotation maps this Java Servlet Class to a URL
 @WebServlet("/stars")
 public class StarServlet extends HttpServlet {
-    private static final long serialVersionUID = 1L;
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         // Change this to your own mysql username and password
         String loginUser = "mytestuser";
-        String loginPasswd = "My6$Password";
+        String loginPasswd = "My6Password";
         String loginUrl = "jdbc:mysql://localhost:3306/moviedbexample";
 
         // Set response mime type
         response.setContentType("text/html");
 
         // Get the PrintWriter for writing response
-        PrintWriter out = response.getWriter();
+        PrintWriter htmlOutput = response.getWriter();
+        htmlOutput.println("<html>");
+        htmlOutput.println("<head><title>Fabflix</title></head>");
+        htmlOutput.println("<body>");
+        htmlOutput.println("<h1>MovieDB Stars</h1>");
+        htmlOutput.println("<table border>");
+        htmlOutput.println("<tr>");
+        htmlOutput.println("<td>id</td>");
+        htmlOutput.println("<td>name</td>");
+        htmlOutput.println("<td>birth year</td>");
+        htmlOutput.println("</tr>");
 
-        out.println("<html>");
-        out.println("<head><title>Fabflix</title></head>");
+        try (Connection connection = DriverManager.getConnection(loginUrl, loginUser, loginPasswd);
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery("SELECT * from stars limit 10");) {
 
-        try {
-            Class.forName("com.mysql.jdbc.Driver").newInstance();
-            // create database connection
-            Connection connection = DriverManager.getConnection(loginUrl, loginUser, loginPasswd);
-            // declare statement
-            Statement statement = connection.createStatement();
-            // prepare query
-            String query = "SELECT * from stars limit 10";
-            // execute query
-            ResultSet resultSet = statement.executeQuery(query);
+            Class.forName("com.mysql.jdbc.Driver");
 
-            out.println("<body>");
-            out.println("<h1>MovieDB Stars</h1>");
-
-            out.println("<table border>");
-
-            // Add table header row
-            out.println("<tr>");
-            out.println("<td>id</td>");
-            out.println("<td>name</td>");
-            out.println("<td>birth year</td>");
-            out.println("</tr>");
-
-            // Add a row for every star result
             while (resultSet.next()) {
-                // get a star from result set
                 String starID = resultSet.getString("id");
                 String starName = resultSet.getString("name");
                 String birthYear = resultSet.getString("birthyear");
-
-                out.println("<tr>");
-                out.println("<td>" + starID + "</td>");
-                out.println("<td>" + starName + "</td>");
-                out.println("<td>" + birthYear + "</td>");
-                out.println("</tr>");
+                htmlOutput.println("<tr>");
+                htmlOutput.println("<td>" + starID + "</td>");
+                htmlOutput.println("<td>" + starName + "</td>");
+                htmlOutput.println("<td>" + birthYear + "</td>");
+                htmlOutput.println("</tr>");
             }
 
-            out.println("</table>");
-            out.println("</body>");
-
-            resultSet.close();
-            statement.close();
-            connection.close();
-
+            htmlOutput.println("</table>");
+            htmlOutput.println("</body>");
         } catch (Exception e) {
             /*
              * After you deploy the WAR file through tomcat manager webpage,
              *   there's no console to see the print messages.
-             * Tomcat append all the print messages to the file: tomcat_directory/logs/catalina.out
+             * Tomcat append all the print messages to the file: tomcat_directory/logs/catalina.htmlOutput
              *
              * To view the last n lines (for example, 100 lines) of messages you can use:
-             *   tail -100 catalina.out
+             *   tail -100 catalina.htmlOutput
              * This can help you debug your program after deploying it on AWS.
              */
             request.getServletContext().log("Error: ", e);
 
-            out.println("<body>");
-            out.println("<p>");
-            out.println("Exception in doGet: " + e.getMessage());
-            out.println("</p>");
-            out.print("</body>");
+            htmlOutput.println("<body>");
+            htmlOutput.println("<p>");
+            htmlOutput.println("Exception in doGet: " + e.getMessage());
+            htmlOutput.println("</p>");
+            htmlOutput.print("</body>");
         }
-
-        out.println("</html>");
-        out.close();
-
+        htmlOutput.println("</html>");
+        htmlOutput.close();
     }
-
-
 }
